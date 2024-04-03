ad7667350078c634d726c7cd7f2ada5f045cc4ec
KadenQMc <kadenmc018@gmail.com>
2024-04-03 13:31:19 -0400

    Reformat Parser

--- stylegan+decoder/train.py
+++ stylegan+decoder/train.py
@@ -306,34 +306,18 @@ if __name__ == '__main__':
                         default='')
     parser.add_argument('--cuda_device', type=int, default=1)
     parser.add_argument('--batch_size', type=int, default=16)
-    parser.add_argument(
-        '--phase',
-        type=int,
-        default=600_000,
-        help='number of samples used for each training phases',
-    )
+    parser.add_argument('--phase', type=int, default=600_000,
+                        help='number of samples used for each training phases')
     parser.add_argument('--lr', default=0.001, type=float, help='learning rate')
     parser.add_argument('--sched', action='store_true', help='use lr scheduling')
     parser.add_argument('--init_size', default=128, type=int, help='initial image size')
     parser.add_argument('--max_size', default=1024, type=int, help='max image size')
-    parser.add_argument(
-        '--ckpt', default=None, type=str, help='load from previous checkpoints'
-    )
-    parser.add_argument(
-        '--no_from_rgb_activate',
-        action='store_true',
-        help='use activate in from_rgb (original implementation)',
-    )
-    parser.add_argument(
-        '--mixing', action='store_true', help='use mixing regularization'
-    )
-    parser.add_argument(
-        '--loss',
-        type=str,
-        default='wgan-gp',
-        choices=['wgan-gp', 'r1'],
-        help='class of gan loss',
-    )
+    parser.add_argument('--ckpt', default=None, type=str, help='load from previous checkpoints')
+    parser.add_argument('--no_from_rgb_activate', action='store_true',
+                        help='use activate in from_rgb (original implementation)')
+    parser.add_argument('--mixing', action='store_true', help='use mixing regularization')
+    parser.add_argument('--loss', type=str, default='wgan-gp', choices=['wgan-gp', 'r1'],
+                        help='class of gan loss')
 
     args = parser.parse_args()
 
