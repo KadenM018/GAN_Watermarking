07b1c6f73329056bc47d1ecc2e682c3ffe3c4bb3
KadenM018 <kadenmc018@gmail.com>
2024-04-25 14:48:27 -0400

    Add Comments

--- stylegan+decoder/test.py
+++ stylegan+decoder/test.py
@@ -3,13 +3,18 @@ import math
 import torchvision
 import torch
 from torch import nn
-from model import StyledGenerator,StegaStampDecoder
+from model import StyledGenerator, StegaStampDecoder
 import os
 from torchvision import transforms
+import glob
+import numpy as np
 
-data_processing = transforms.ColorJitter(0, 0, 0, 0)
+data_processing = transforms.ColorJitter(0, 0, 0, 0)  # (brightness, contrast, saturation, hue)
 
 parser = argparse.ArgumentParser(description='Progressive Growing of GANs')
+
+parser.add_argument('--dataset_path', type=str, default='/home/kaden/Downloads/archive/img_align_celeba/img_align_celeba',
+                    help='Path the folder with all dataset images')
 
 parser.add_argument('--size', type=int, default=128,
                     help='Size of the generated image. For CelebA this should be 128 and for flower dataset it should be 256')
@@ -17,7 +22,7 @@ parser.add_argument('--size', type=int, default=128,
 parser.add_argument('--num_samples', type=int, default=16,
                     help='Number of samples to generate')
 
-parser.add_argument('--device', type=str, default='cpu',
+parser.add_argument('--device', type=str, default='cuda:0',
                     help='Write cuda:<device_num> for gpu or cpu for cpu')
 
 parser.add_argument('--decoder_weights', type=str,
@@ -39,9 +44,9 @@ parser.add_argument('--save_path', type=str,
 args = parser.parse_args()
 
 code_size = 512
-b_size = args.num_samples
 step = int(math.log2(args.size)) - 2
 
+# Create generator, load weights into memory, then load weights into the generator
 if args.device[0:4] == 'cuda':
     generator = nn.DataParallel(StyledGenerator(code_size), device_ids=[int(args.device[-1])]).to(args.device)
     ckpt = torch.load(args.stylegan_weights, map_location=args.device)
@@ -51,26 +56,28 @@ else:
     ckpt = torch.load(args.stylegan_weights, map_location=args.device)
     generator.load_state_dict(ckpt['generator'])
 
-decoder = StegaStampDecoder(resolution=args.size, IMAGE_CHANNELS=3, fingerprint_size=100)
-weights = torch.load(args.decoder_weights, map_location=args.device)
-decoder.load_state_dict(weights)
-decoder.to(args.device)
-decoder.eval()
+decoder = StegaStampDecoder(resolution=args.size, IMAGE_CHANNELS=3, fingerprint_size=100)  # Create decoder
+weights = torch.load(args.decoder_weights, map_location=args.device)  # load weights into memory
+decoder.load_state_dict(weights)  # load weights into decoder model
+decoder.to(args.device)  # send decoder to the given device
+decoder.eval()  # put decoder in evaluation mode (performs differently in training mode)
 
-fingerprints = torch.load(args.fingerprints).to(args.device)
+fingerprints = torch.load(args.fingerprints).to(args.device)  # Load watermark into memory
 
-gen_in1, gen_in2 = torch.randn(2, b_size, code_size, device=args.device).chunk(
-                2, 0
-            )
-gen_in1 = gen_in1.squeeze(0)
+# Generate random noise
+_, gen_in2 = torch.randn(2, args.num_samples, code_size, device=args.device).chunk(2, 0)
 gen_in2 = gen_in2.squeeze(0)
 
+# Give random noise to generator and save output
 fake_image = generator(gen_in2, step=step, alpha=1)
 
+# Do image processing on the image from the generator
 altered_images = data_processing(fake_image)
 
+# Send altered image to the decoder and save output
 decoder_output = decoder(altered_images)
 
+# Calculate binary cross-entropy loss (BCE) between predicted watermark and actual watermark
 criterion = nn.BCEWithLogitsLoss()
 BCE_loss = 0
 for j in range(0, decoder_output.size(0)):
@@ -78,6 +85,7 @@ for j in range(0, decoder_output.size(0)):
 BCE_loss = BCE_loss / decoder_output.size(0)
 print(f'\nBCE_loss: {BCE_loss}')
 
+# Convery predicted watermark to binary numbers and calculate bitwise accuracy
 fingerprints_predicted = (decoder_output > 0).float()
 bitwise_accuracy = 1.0 - torch.mean(
     torch.abs(fingerprints - fingerprints_predicted)
@@ -85,4 +93,14 @@ bitwise_accuracy = 1.0 - torch.mean(
 
 print(f'\nbitwise accuracy: {bitwise_accuracy}')
 
+# Save samples as a batch in one image
 torchvision.utils.save_image(fake_image, os.path.join(args.save_path, 'sample.png'), normalize=True)
+
+# Save samples as individual images
+# import matplotlib.pyplot as plt
+# for i, image in enumerate(fake_image):
+#     img = image.permute(1, 2, 0).detach().cpu().numpy()
+#     img = (img - np.min(img)) / (np.max(img) - np.min(img))
+#     img *= 255
+#     img = np.uint8(img)
+#     plt.imsave(os.path.join(args.save_path, f'sample_{i}.png'), img)
